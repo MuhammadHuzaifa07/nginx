23c335b57249104bd7c1a4568ff17868ef84eeb3
MuhammadHuzaifa07 <121166252+MuhammadHuzaifa07@users.noreply.github.com>
2022-12-21 21:36:43 +0500

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Version</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">PHP Version</h1>
+    <p style="text-align: center;">
+        <?php echo "The current version of PHP is " . phpversion(); ?>
+    </p>
+</body>
+
+</html>
